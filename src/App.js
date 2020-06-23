95494cb23555abfc5c4abfe7ba9efe3a1f9e32f2
tspmwant <tspm.wantoro@gmail.com>
2020-06-23 11:27:34 +0700

    coba edit dikit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 function App() {
-  return <div>Test react</div>;
+  return (
+    <div>
+      <h1>Test react</h1>
+      <p>coba edit reaact</p>
+    </div>
+  );
 }
 
 export default App;
